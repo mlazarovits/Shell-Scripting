f10ad33ee29a1f909234e02a68f8e6bd7951e718
Margaret Lazarovits <mlazarovits@gmail.com>
2020-05-26 18:18:45 -0500

    updated installation method

--- morningProgram/install.py
+++ morningProgram/install.py
@@ -5,29 +5,54 @@
 import os
 from run import *
 
+
+
 HOME = os.getenv("HOME")
 PATH = os.getcwd()
 
-def sh(script, msg = 0):
-  os.system("bash -c '%s'" % script)
+# def sh(script, msg = 0):
+#   os.system("bash -c '%s'" % script)
 
-if not os.path.exists("done.txt"): # hacky way to tell the install.py hasn't already been run
-  print("Installing 'send' program into bash...")
-  os.chdir(HOME)
+#figure out a better way to check installation
+os.chdir(HOME)
+bashFile = open(".bash_profile","r")
+bashLines = bashFile.readlines()
+bashFile.close()
+bashFile = open(".bash_profile", "a")
+f = open(PATH + "/bash_function.txt", "r")
 
-  b = open(".bash_profile", "a")
-  f = open(PATH + "/bash_function.txt", "r")
-  for line in f:
-    b.write(line)
-  b.write("  osascript " + PATH + "/sendMessage.applescript \"$contact\" \"$msg\"\n")
-  b.write("}\n")
-  b.write("export -f send\n")
+found = False
+print("Installing 'send' program into bash...")
+for line in f:
+  if line not in bashLines:
+    bashFile.write(line)  
+  else: 
+    found = True
 
-  r = open(PATH + "/done.txt", "a")
-  r.write("Installation complete.\n")
 
-  sh("source .bash_profile")
-  os.chdir(PATH)
+
+# if not os.path.exists("done.txt"): # hacky way to tell the install.py hasn't already been run
+  # print("Installing 'send' program into bash...")
+  # os.chdir(HOME)
+
+  # b = open(".bash_profile", "a")
+  # f = open(PATH + "/bash_function.txt", "r")
+  # for line in f:
+  #   b.write(line)
+  # b.write("  osascript " + PATH + "/sendMessage.applescript \"$contact\" \"$msg\"\n")
+  # b.write("}\n")
+  # b.write("export -f send\n")
+
+  # r = open(PATH + "/done.txt", "a")
+  # r.write("Installation complete.\n")
+bashFile.close()
+
+if found:
+  print("Already Installed")
+sh("source .bash_profile")
+os.chdir(PATH)
+# else:
+#   print("Already installed")
 
 print("---")
 print("Installed sending capabilities (iMessage only).")
