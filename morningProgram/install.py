22348eff8015c074d2d17bb705ba662e8bb7e298
Margaret Lazarovits <mlazarovits@gmail.com>
2020-05-26 19:26:08 -0500

    clean up

--- morningProgram/install.py
+++ morningProgram/install.py
@@ -3,61 +3,43 @@
 # sending function in the .bash_profile file
 
 import os
-from run import *
 
 
+def sh(script, msg = 0):
+  os.system("bash -c '%s'" % script)
 
-HOME = os.getenv("HOME")
-PATH = os.getcwd()
-
-# def sh(script, msg = 0):
-#   os.system("bash -c '%s'" % script)
-
-#figure out a better way to check installation
-os.chdir(HOME)
-bashFile = open(".bash_profile","r")
-bashLines = bashFile.readlines()
-bashFile.close()
-bashFile = open(".bash_profile", "a")
-f = open(PATH + "/bash_function.txt", "r")
-
-found = False
-print("Installing 'send' program into bash...")
-for line in f:
-  if line not in bashLines:
-    bashFile.write(line)  
-  else: 
-    found = True
+def install():
+  HOME = os.getenv("HOME")
+  PATH = os.getcwd()
 
 
+  os.chdir(HOME)
+  bashFile = open(".bash_profile","r")
+  bashLines = bashFile.readlines()
+  bashFile.close()
+  bashFile = open(".bash_profile", "a")
+  f = open(PATH + "/bash_function.txt", "r")
 
-# if not os.path.exists("done.txt"): # hacky way to tell the install.py hasn't already been run
-  # print("Installing 'send' program into bash...")
-  # os.chdir(HOME)
+  found = False
+  print("Installing 'send' program into bash...")
+  for line in f:
+    if line not in bashLines:
+      bashFile.write(line)  
+    else: 
+      found = True
 
-  # b = open(".bash_profile", "a")
-  # f = open(PATH + "/bash_function.txt", "r")
-  # for line in f:
-  #   b.write(line)
-  # b.write("  osascript " + PATH + "/sendMessage.applescript \"$contact\" \"$msg\"\n")
-  # b.write("}\n")
-  # b.write("export -f send\n")
+  bashFile.close()
 
-  # r = open(PATH + "/done.txt", "a")
-  # r.write("Installation complete.\n")
-bashFile.close()
+  if found:
+    print("Already Installed")
+  sh("source .bash_profile")
+  os.chdir(PATH)
 
-if found:
-  print("Already Installed")
-sh("source .bash_profile")
-os.chdir(PATH)
-# else:
-#   print("Already installed")
 
-print("---")
-print("Installed sending capabilities (iMessage only).")
-print("Please start a new terminal window (Cmd-T) to test the new capabilities.")
-print("To send a message, type: <send \"Contact Name\" \"Message\"> in the command line.")
-print("Alternatively, you can mess with the 'run.py' function to schedule your own messages.")
-print("---")
-print("By Gabe Schoenbach")
+  print("---")
+  print("Installed sending capabilities (iMessage only).")
+  print("Please start a new terminal window (Cmd-T) to test the new capabilities.")
+  print("To send a message, type: <send \"Contact Name\" \"Message\"> in the command line.")
+  print("Alternatively, you can mess with the 'run.py' function to schedule your own messages.")
+  print("---")
+  print("By Gabe Schoenbach")
