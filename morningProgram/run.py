c47e2c10551ba853f4b93e5c6f616130f5d08390
Margaret Lazarovits <mlazarovits@gmail.com>
2020-05-26 19:26:18 -0500

    added command line args

--- morningProgram/run.py
+++ morningProgram/run.py
@@ -1,21 +1,13 @@
 import os
+import sys
 from datetime import datetime
 import time
 import random
+import argparse
+from install import sh
+
 
 def main():
-  def sh(script, msg = 0):
-      os.system("bash -c '%s'" % script)
-
-#  sh("echo Running bash in Python...")
- 
-  contacts = ['"Gabe Schoenbach"', '"Daven Crossland"',
-              '"Helena Thomas"', '"Alex Cintron"',
-              '"Erika Steiner"', '"Scott Ellenoff"',
-              '"Kahlan Lee-Lermer"', '"Rowan Schneider"',
-              '"Jackson Mariotti"', '"Jake Gosselin"',
-              '"Ken Ito"']
-
   greetings = ['"Good morning!"', 
                '"morning bud!"', 
                '"Good morning! Did you sleep well?"',
@@ -34,24 +26,38 @@ def main():
                '"Wishing you a day full of sunny smiles and happy thoughts. Good morning!"',
                '"This is not just another day. It is yet another chance to make your dreams come true. Get up and get started. Good morning!"']
 
-  random.shuffle(greetings)
+
+
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('contacts',type=str,nargs='+',help='contacts to text')
+  parser.add_argument('-m','--message',metavar='message',type=str,default=random.choice(greetings),help='message to send')
+  args = parser.parse_args()
+
 
   now = datetime.now()
   current_time = now.strftime("%H:%M:%S")
+  send_hr = '08'
   print("Started scheduling at:", current_time) 
-  print("Will send messages to:", [friend for friend in contacts])
- 
-  i = 0;
-  while True:
+  print("Will send {} to {} within the hour of {}".format(args.message,args.contacts,send_hr))
+
+
+  sent = False;
+  while not sent:
     now = datetime.now().strftime("%H:%M:%S")
-    if now[:2] == '08': 
-      for friend in contacts:
-        sh('send ' + friend + ' ' + greetings[i % len(greetings)])
-        continue;  
-      time.sleep(3600) 
-      i += 1
+    if now[:2] == send_hr: 
+      for friend in args.contacts:
+        sh('send "' + friend + '" "' + args.message+'" ')
+        print('Sent message(s)!')
+        sent = True
+        return
     else:
       print("Checked time at: ", now) 
-      time.sleep(3600)
+      time.sleep(3600) #check every hour
+
+
+
+
+
 
 main()
